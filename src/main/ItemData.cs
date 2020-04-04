ddcaa4f7dce9913178597556b7a9747bc619de7d
Elmer Roderick Bool <elmer.bool@ei8.works>
2020-04-05 00:21:46 +1100

    Initial commit

---
+++ src/main/ItemData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace works.ei8.Data.Tag.Common
+{
+    public class ItemData
+    {
+        public string Id { get; set; }
+
+        public string Tag { get; set; }
+
+        public int Version { get; set; }
+    }
+}
